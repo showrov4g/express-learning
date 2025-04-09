628d260e5692c1fdc5edb1fb19af2a0669dcc8f6
showrov4g <showrovg00@gmail.com>
2025-04-09 21:12:25 +0600

    making route in other folder

--- index.js
+++ index.js
@@ -6,32 +6,15 @@ const app = express();
 const PORT = 3000;
 
 
-// define a simple route 
-
-app.get('/',(req, res)=>{
+// index route 
+app.get('/', (req, res) => {
     res.send("hello ghosh")
 })
-// second  route
-app.get('/about',(req, res)=>{
-    res.send("this is about api routes")
-})
-// third rouutes 
-app.get('/contact',(req, res)=>{
-    res.send("this is contact api routes")
-})
 
-// making a dynamic routes 
 
-app.get('/user/:username', (req,res)=>{
-    const username = req.params.username;
-    res.send(`Welcome mr: ${username}`)
-})
+/
 
-// make daynamic serarch api 
-app.get("/search" ,  (req,res)=>{
-    const keyword = req.query.keyword;
-    res.send(`sercing for ${keyword}`)
-})
+
 
 
 
@@ -39,6 +22,6 @@ app.get("/search" ,  (req,res)=>{
 
 
 // ===============
-app.listen(PORT, ()=>{
+app.listen(PORT, () => {
     console.log(`Server is running on PORT ${PORT}`)
 })
