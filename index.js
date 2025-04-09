00b2e031c5c5411e167396d62735ff6566931728
showrov4g <showrovg00@gmail.com>
2025-04-09 11:39:03 +0600

    simple api making

--- index.js
+++ index.js
@@ -11,7 +11,14 @@ const PORT = 3000;
 app.get('/',(req, res)=>{
     res.send("hello ghosh")
 })
-
+// second  route
+app.get('/about',(req, res)=>{
+    res.send("this is about api routes")
+})
+// third rouutes 
+app.get('/contact',(req, res)=>{
+    res.send("this is contact api routes")
+})
 
 app.listen(PORT, ()=>{
     console.log(`Server is running on PORT ${PORT}`)
