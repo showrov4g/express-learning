862d3522bfd382ce18b71dc7f5cfcf9353e1cbe2
showrov4g <showrovg00@gmail.com>
2025-06-12 10:06:49 +0600

    learning express

--- dist/app.js
+++ dist/app.js
@@ -4,9 +4,40 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const fs_1 = __importDefault(require("fs"));
+const path_1 = __importDefault(require("path"));
 const app = (0, express_1.default)();
 const port = 3000;
+// ------------
+app.use(express_1.default.json());
+const todoRouter = express_1.default.Router();
+const userRouter = express_1.default.Router();
+app.use("/", todoRouter);
+app.use('/users', userRouter);
+// -----------
+const filePath = path_1.default.join(__dirname, "../db/todo.json");
 app.get('/', (req, res) => {
+    console.log({ req, res });
+    res.send('welcome to your todo app');
+});
+todoRouter.get('/todos', (req, res) => {
+    console.log("From params", req.params);
+    const data = fs_1.default.readFileSync(filePath, { encoding: "utf-8" });
+    // console.log(data)
+    res.json({
+        message: "todo router",
+        data
+    });
+});
+// app.get('/todos', (req: Request, res : Response) => {
+//   console.log("From params", req.params)
+//     const data = fs.readFileSync(filePath, {encoding: "utf-8"});
+//     // console.log(data)
+//   res.json(data);
+// })
+app.post('/todos/create-todos', (req, res) => {
+    const { body } = req.body;
+    console.log(body);
     res.send('Hello World!');
 });
 app.listen(port, () => {
--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,51 @@
 import  express, { Application, Request, Response }  from "express"
+import fs from "fs";
+import path from "path";
 const app: Application = express()
 const port = 3000
+// ------------
+app.use(express.json())
+
+const todoRouter = express.Router();
+const userRouter = express.Router();
+
+
+app.use("/", todoRouter);
+app.use('/users', userRouter);
+
+
+// -----------
+const filePath = path.join(__dirname , "../db/todo.json")
 
 app.get('/', (req: Request, res : Response) => {
-  res.send('Hello World!')
+
+  console.log({req, res})
+  res.send('welcome to your todo app')
 })
-app.get('/todos', (req: Request, res : Response) => {
-  res.send('Hello World!')
+todoRouter.get('/todos', (req: Request, res : Response) => {
+  console.log("From params", req.params)
+    const data = fs.readFileSync(filePath, {encoding: "utf-8"});
+    // console.log(data)
+
+  res.json({
+    message: "todo router",
+    data
+  });
 })
+
+
+// app.get('/todos', (req: Request, res : Response) => {
+//   console.log("From params", req.params)
+//     const data = fs.readFileSync(filePath, {encoding: "utf-8"});
+//     // console.log(data)
+
+
+//   res.json(data);
+// })
 app.post('/todos/create-todos', (req: Request, res : Response) => {
+  const {body} = req.body;
+  console.log(body);
+  
   res.send('Hello World!')
 })
 
