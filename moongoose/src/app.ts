3697116b96e5e79fcf57118edc440ec6aaceb8e7
showrov4g <showrovg00@gmail.com>
2025-06-18 07:37:19 +0600

    learning moongose

--- moongoose/src/app.ts
+++ moongoose/src/app.ts
@@ -2,16 +2,37 @@ import express, { Application, Request, Response } from "express"
 import { model, Schema } from "mongoose";
 
 const app: Application = express();
+app.use(express.json());
 
 // schema 
 const noteSchema = new Schema({
-    title: String,
-    content: String
+    title: {type: String, required: true, trim: true},
+    content: {type: String, },
+    category: {
+        type: String,
+        enum: ["personal", "work", "study", "other"],
+        default: "personal"
+    },
+    pinned:{
+        type : "Boolean",
+        default: false,
+    }
 })
 
 const Note = model("Note", noteSchema);
 
-app.post("/")
+app.post("/create-note", async(req: Request, res: Response)=>{
+    const myNote = new Note({
+        title: "learning mongoose",
+        content: "I am learning mongoose"
+    })
+    await myNote.save();
+    res.status(201).json({
+        success: true,
+        message:"Successfully created",
+        myNote
+    })
+})
 
 app.get('/', (req: Request, res: Response) => {
     res.send("Welcome to my Note app")
--- moongoose/src/server.ts
+++ moongoose/src/server.ts
@@ -7,7 +7,7 @@ let server: Server;
 const PORT = 5000;
 async function main(){
     try {
-         await mongoose.connect('mongodb+srv://learningexpress:Ba1ALs83Ccd0uWJK@cluster0.23lvn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
+         await mongoose.connect('mongodb+srv://learningexpress:Ba1ALs83Ccd0uWJK@cluster0.23lvn.mongodb.net/advance-to-do-app?retryWrites=true&w=majority&appName=Cluster0');
          console.log("Connected to mongodb with mongoose")
         server = app.listen(PORT,()=>{
             console.log(`app is running on port ${PORT}`)
