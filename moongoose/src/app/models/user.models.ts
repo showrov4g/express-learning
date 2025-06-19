ea208ce7a13cc74a40a2835fdbae9e79fe6c1379
showrov4g <showrovg00@gmail.com>
2025-06-19 08:02:34 +0600

    user schema making

---
+++ moongoose/src/app/models/user.models.ts
@@ -0,0 +1,28 @@
+import { model, Schema } from "mongoose";
+import { IUser } from "../interfaces/user.interface";
+
+const userSchema = new Schema<IUser>({
+    firstName:{
+        type: String,
+        required: true,
+        trim: true,
+    },
+  lastName:{
+        type: String,
+        required: true,
+        trim: true,
+    },
+
+    email:{
+        type: String,
+        required: true,
+        trim: true,
+    },
+    role:{
+        type: String,
+        enum:["user", "admin"],
+        default: "user"
+    }
+})
+
+export const User = model("user", userSchema);
